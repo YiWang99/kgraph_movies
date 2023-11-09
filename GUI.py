729638c453cbeeebe6887e577567008820dfe789
ueaul <pstapf22@gmail.com>
2023-11-09 18:00:25 +0100

    updated gui

--- GUI.py
+++ GUI.py
@@ -7,27 +7,28 @@ from PIL import Image, ImageTk
 def filterSelection(e):
     selectedFilter = filterMenu.get()
     if selectedFilter == "Duration" or selectedFilter == "Release Year" or selectedFilter == "Content Rating":
-        actorGenreInputText.grid_forget()
-        comparisonSignMenu.grid(row=0, column=0, padx="10", pady="70", sticky="nw")
-        durationRealeaseInputText.grid(row=0, column=0, padx="180", pady="70", sticky="nw")
+        actorGenreInputText.place_forget()
+        comparisonSignMenu.place(x=10, y=90)
+        durationRealeaseInputText.place(x=150, y=90)
     else:
-        comparisonSignMenu.grid_forget()
-        durationRealeaseInputText.grid_forget()
-        actorGenreInputText.grid(row=0, column=0, padx="10", pady="70", sticky="nw")
+        comparisonSignMenu.place_forget()
+        durationRealeaseInputText.place_forget()
+        actorGenreInputText.place(x=10, y=90)
 
 def addFilter():
     selectedIndex = filterMenu.current()
     if selectedIndex == 2 or selectedIndex == 3 or selectedIndex == 11:
         input = durationRealeaseInputText.get(1.0, tk.END).strip()
         comperator = comparisonSignMenu.get().strip()
-        filterTexts[selectedIndex].configure(state="normal")
-        print(len(filterTexts[selectedIndex].get(1.0, tk.END)))
-        if len(filterTexts[selectedIndex].get(1.0, tk.END)) == 1:
-            filterTexts[selectedIndex].insert("insert", comperator + " " + input)
-        else:
-            filterTexts[selectedIndex].insert("insert", ";" + comperator + " " + input)
-        filterTexts[selectedIndex].configure(state="disabled")
-        durationRealeaseInputText.delete(1.0,tk.END)
+        if input != "" and comperator != "Comparator":
+            filterTexts[selectedIndex].configure(state="normal")
+            print(len(filterTexts[selectedIndex].get(1.0, tk.END)))
+            if len(filterTexts[selectedIndex].get(1.0, tk.END)) == 1:
+                filterTexts[selectedIndex].insert("insert", comperator + " " + input)
+            else:
+                filterTexts[selectedIndex].insert("insert", ";" + comperator + " " + input)
+            filterTexts[selectedIndex].configure(state="disabled")
+            durationRealeaseInputText.delete(1.0,tk.END)
     else:
         input = actorGenreInputText.get(1.0, tk.END).strip()
         filterTexts[selectedIndex].configure(state="normal")
@@ -40,12 +41,13 @@ def addFilter():
 
 def searchMovies():
     filterArray = []
+    filterNames = np.array(["actor", "genre", "runtime", "initial_release_date", "director", "producer", "language",
+                            "writer", "editor", "music_contributor", "country"])
     index = 0
     for filter in filterTexts:
         rawString = filter.get(1.0,tk.END).strip()
         elements = rawString.split(";")
-        label = filterLabels[index].cget("text")
-        label = label[:-1]
+        label = filterNames[index]
         elements.insert(0, label)
         rowArray = np.array(elements, dtype="str")
         filterArray.append(rowArray)
@@ -108,10 +110,7 @@ def clearMusicContributorsFilter():
     musicContributorsFilters.delete(1.0,tk.END)
     musicContributorsFilters.configure(state="disabled")
 
-def clearContentRatingsFilter():
-    contentRatingFilters.configure(state="normal")
-    contentRatingFilters.delete(1.0,tk.END)
-    contentRatingFilters.configure(state="disabled")
+
 
 #Create and set up rootWindow
 rootWindow = tk.Tk()
@@ -135,28 +134,32 @@ background_label.place(relwidth=1, relheight=1)
 bold_font = font.nametofont("TkDefaultFont")
 bold_font.configure(weight="bold")
 
-#create labels
-addFilterLabel = tk.Label(rootWindow, text="Add Filter", width=12, font=("Arial", 20, "bold"), background=color6)
-appliedFiltersLabel = tk.Label(rootWindow, text="Applied Filters", width=12, font=("Arial", 20, "bold"), background=color6)
-movieLabel = tk.Label(rootWindow, text="Movie", width=12, font=("Arial", 20, "bold"), background=color6)
-abstractLabel = tk.Label(rootWindow, text="Abstract", width=12, font=("Arial", 20, "bold"), background=color6)
+#create canva with headlines
+canvas = tk.Canvas(rootWindow, width=1400, height=900, bg=rootWindow.cget('bg'), highlightthickness=0)
+canvas.place(x=0, y=0)
+backgroundImage = Image.open("ressources/background.jpg")
+background = ImageTk.PhotoImage(backgroundImage)
+canvas.create_image(700, 450, anchor=tk.CENTER, image=background)
+canvas.create_text(95, 30, text="Add Filters", font="arial 25 bold", fill="white")
+canvas.create_text(125, 200, text="Applied Filters", font="arial 25 bold", fill="white")
+canvas.create_text(814, 30, text="Movies", font="arial 25 bold", fill="white")
+canvas.create_text(826, 200, text="Abstract", font="arial 25 bold", fill="white")
 
-actorsLabel = tk.Label(rootWindow, text="Actors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-genresLabel = tk.Label(rootWindow, text="Genres:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-durationLabel = tk.Label(rootWindow, text="Duration:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-releaseYearLabel = tk.Label(rootWindow, text="Release:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-directorsLabel = tk.Label(rootWindow, text="Directors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-distributorsLabel = tk.Label(rootWindow, text="Distributors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-producersLabel = tk.Label(rootWindow, text="Producers:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-languagesLabel = tk.Label(rootWindow, text="Languages:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-writersLabel = tk.Label(rootWindow, text="Writers:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-editorsLabel = tk.Label(rootWindow, text="Editors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
-musicContributorsLabel = tk.Label(rootWindow, text="Music Contributors:", width=10, anchor="w", foreground="white", background=color4, font=("Arial", 19, "bold"))
-contentRatingLabel = tk.Label(rootWindow, text="Content Rating:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 19, "bold"))
+#create labels
+actorsLabel = tk.Label(rootWindow, text="Actors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+genresLabel = tk.Label(rootWindow, text="Genres:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+durationLabel = tk.Label(rootWindow, text="Duration:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+releaseYearLabel = tk.Label(rootWindow, text="Release:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+directorsLabel = tk.Label(rootWindow, text="Directors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+distributorsLabel = tk.Label(rootWindow, text="Distributors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+producersLabel = tk.Label(rootWindow, text="Producers:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+languagesLabel = tk.Label(rootWindow, text="Languages:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+writersLabel = tk.Label(rootWindow, text="Writers:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+editorsLabel = tk.Label(rootWindow, text="Editors:", width=10, anchor="w", background=color4, foreground="white", font=("Arial", 15, "bold"))
+musicContributorsLabel = tk.Label(rootWindow, text="Composer:", width=10, anchor="w", foreground="white", background=color4, font=("Arial", 15, "bold"))
 
 filterLabels = np.array([actorsLabel, genresLabel, durationLabel, releaseYearLabel, directorsLabel, distributorsLabel,
-                producersLabel, languagesLabel, writersLabel, editorsLabel, musicContributorsLabel,
-                contentRatingLabel])
+                producersLabel, languagesLabel, writersLabel, editorsLabel, musicContributorsLabel])
 
 
 #create buttons
@@ -166,7 +169,9 @@ searchButton = tk.Button(rootWindow, text="Search", command=searchMovies, backgr
 findMoreButton = tk.Button(rootWindow, text="Find More", background=color4, foreground="white",
                               activebackground=color5, border = 0, width=10, height=2, cursor = "hand2",
                               font=("Arial", 16, "bold"), activeforeground="white")
-addFilterButton = tk.Button(rootWindow, text="Add", command=addFilter)
+addFilterButton = tk.Button(rootWindow, text="Add", command=addFilter, background=color4, foreground="white",
+                              activebackground=color5, border=0, width=5, height=1, cursor = "hand2",
+                              font=("Arial", 14, "bold"), activeforeground="white")
 
 clearActorsButton = tk.Button(rootWindow, text="Clear", command=clearActorsFilter, background=color1,
                               activebackground=color2, border = 0, width=5, height=0, cursor = "hand2",
@@ -201,79 +206,64 @@ clearEditorButton = tk.Button(rootWindow, text="Clear", command=clearEditorsFilt
 clearMusicContributorButton = tk.Button(rootWindow, text="Clear", command=clearMusicContributorsFilter, background=color1,
                               activebackground=color2, border = 0, width=5, height=0, cursor = "hand2",
                               font=("Arial", 14, "bold"))
-clearContentRatingButton = tk.Button(rootWindow, text="Clear", command=clearContentRatingsFilter, background=color1,
-                              activebackground=color2, border = 0, width=5, height=0, cursor = "hand2",
-                              font=("Arial", 14, "bold"))
+
 
 clearButtons = np.array([clearActorsButton, clearGenresButton, clearDurationButton, clearReleaseButton,
                          clearDirectorButton ,clearDistributorButton, clearProducerButton, clearLanguageButton,
-                         clearWriterButton, clearEditorButton, clearMusicContributorButton, clearContentRatingButton])
+                         clearWriterButton, clearEditorButton, clearMusicContributorButton])
 
 
 #create text fields
-actorGenreInputText = tk.Text(rootWindow, height=1, width=100)
-durationRealeaseInputText = tk.Text(rootWindow, height=1, width=100)
+actorGenreInputText = tk.Text(rootWindow, height=1, width=72)
+durationRealeaseInputText = tk.Text(rootWindow, height=1, width=54)
 
-abstractText = tk.Text(rootWindow, height=100, width=100)
+abstractText = tk.Text(rootWindow, height=33, width=78)
 
-actorsFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-genresFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-durationFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-releaseYearFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-directorsFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-distributorsFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-producersFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-languagesFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-writersFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-editorsFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-musicContributorsFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
-contentRatingFilters = tk.Text(rootWindow, height=2, width=140, state="disabled")
+actorsFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+genresFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+durationFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+releaseYearFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+directorsFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+distributorsFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+producersFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+languagesFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+writersFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+editorsFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
+musicContributorsFilters = tk.Text(rootWindow, height=2, width=54, state="disabled")
 
 filterTexts = np.array([actorsFilters, genresFilters, durationFilters, releaseYearFilters, directorsFilters, distributorsFilters,
-           producersFilters, languagesFilters, writersFilters, editorsFilters, musicContributorsFilters,
-           contentRatingFilters])
+           producersFilters, languagesFilters, writersFilters, editorsFilters, musicContributorsFilters])
 
 #create dropdown menus
 movieOptions = ["Godfather", "Apocalypse Now", "Dune", "Forrest Gump"]
 movieMenu = ttk.Combobox(rootWindow, values=movieOptions, state="readonly")
 movieMenu.set("Movie List")
-filterOptions = ["Actor", "Genre", "Duration", "Release Year", "Director", "Distributor", "Producer", "Language",
-                 "Writer", "Editor", "Music Contributr", "Content Rating"]
-filterMenu = ttk.Combobox(rootWindow, values=filterOptions, state="readonly")
+filterOptions = ["Actor", "Genre", "Duration", "Release", "Director", "Distributor", "Producer", "Language",
+                 "Writer", "Editor", "Composer"]
+filterMenu = ttk.Combobox(rootWindow, values=filterOptions, state="readonly", width=17)
 filterMenu.set("Choose Filter")
 filterMenu.bind("<<ComboboxSelected>>", filterSelection)
 comparisonOptions = ["<", "<=", "=", "=>", ">"]
-comparisonSignMenu = ttk.Combobox(rootWindow, values=comparisonOptions, state="readonly")
-comparisonSignMenu.set("Select")
+comparisonSignMenu = ttk.Combobox(rootWindow, values=comparisonOptions, state="readonly", width=17)
+comparisonSignMenu.set("Comparator")
 
 #add content to grid
-yPos = 50
+yPos = 240
 for i in range(filterLabels.size):
-    filterLabels[i].grid(row=1, column=0, padx=10, pady=str(yPos), sticky="nw")
-    filterTexts[i].grid(row=1, column=0, padx="180", pady=str(yPos), sticky="nw")
-    clearButtons[i].grid(row=1, column=0, padx="10", pady=str(yPos), sticky="ne")
+    rectangle = canvas.create_rectangle(10,yPos, 136,yPos+36,fill=color4, outline="")
+    filterLabels[i].place(x=10, y=str(yPos+3))
+    filterTexts[i].place(x=150, y=str(yPos))
+    clearButtons[i].place(x=600, y=str(yPos))
     yPos += 50
 
-addFilterLabel.grid(row=0, column=0, padx="10", pady="10", sticky="n")
-filterMenu.grid(row=0, column=0, padx="10", pady="40", sticky="nw")
-addFilterButton.grid(row=0, column=0, padx="10", pady="40", sticky="ne")
-appliedFiltersLabel.grid(row=1, column=0, padx="10", pady="10", sticky="n")
-searchButton.grid(row=2, column=0, padx="10", pady="10", sticky="n")
-movieLabel.grid(row=0, column=1, padx="10", pady="10", sticky="n")
-movieMenu.grid(row=0, column=1, padx="10", pady="40", sticky="nw")
-abstractLabel.grid(row=1, column=1, padx="10", pady="10", sticky="n")
-abstractText.grid(row=1, column=1, padx="10", pady="50", sticky="nw")
-findMoreButton.grid(row=2, column=1, padx="10", pady="10", sticky="n")
-
-rootWindow.grid_rowconfigure(0, minsize=30)
-rootWindow.grid_rowconfigure(1, minsize=630)
-rootWindow.grid_rowconfigure(2, minsize=100)
+filterMenu.place(x=10, y=60)
+addFilterButton.place(x=600, y=75)
+searchButton.place(x=300, y=800)
+movieMenu.place(x=760, y=60)
+abstractText.place(x=760, y=240)
+findMoreButton.place(x=1000, y=800)
 
 
-for i in range(2):
-    rootWindow.grid_rowconfigure(i, weight=1, uniform="row")
-for i in range(2):
-    rootWindow.grid_columnconfigure(i, weight=2, uniform="column")
 
 
 rootWindow.mainloop()
