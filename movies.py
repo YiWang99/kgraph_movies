5c5f2312d023d8bf16ee21d7aa351938e473d4fc
LYY-y <1312152518@qq.com>
2023-11-13 22:31:24 +0100

    finish backend

--- movies.py
+++ movies.py
@@ -1,13 +1,16 @@
 from rdflib import Graph, Namespace
 from rdflib.plugins.sparql import prepareQuery
 import time
+import numpy as np
 
 lmdb = Namespace("http://data.linkedmdb.org/movie/")
+lingvo = Namespace("http://www.lingvoj.org/lingvo/")
 initNs = {
             "foaf": "http://xmlns.com/foaf/0.1/", 
             "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#", 
             "rdfs": "http://www.w3.org/2000/01/rdf-schema#", 
             "lmdb": lmdb, 
+            "lingvo": lingvo,
             "xsd": "http://www.w3.org/2001/XMLSchema#",
             "dc": "http://purl.org/dc/terms/"
           }
@@ -26,7 +29,6 @@ def load_kg(file, format='nt'):
     else:
         print("RDF load failed.")
     print(f"load {len(g)} triples.")
-    
     return g
 
 def create_filter_dict(front_end_list:list):
@@ -59,9 +61,14 @@ def generate_query_in_filter(filter_dict):
             continue
         if key in ["actor", "genre", "director", "producer", "writer", "editor", "music_contributor"]:
             for index, value in enumerate(values):
-                q += f"FILTER (?{key}_name{index} = \"{value}\") ."
+                # q += f"FILTER (?{key}_name{index} = \"{value}\") ."
+                q += f"FILTER (STRSTARTS(?{key}_name{index}, \"{value}\")) ."
         else:
-            if key in ["runtime", "initial_release_date"]:
+            if key == "runtime":
+                # maybe multiple values of runtime
+                for value in values:
+                    q += f"FILTER (?{key}_int {value}) ."
+            elif key == "initial_release_date":
                 q += f"FILTER (?{key}_int {values[0]}) ."
             else:
                 q += f"FILTER (?{key} = \"{values[0]}\") ."
@@ -85,9 +92,7 @@ def generate_query_in_where(filter_dict):
     if filter_dict.get("initial_release_date")[0] != "":
         q += "BIND(STRDT(SUBSTR(?initial_release_date, 1, 4), xsd:integer) AS ?initial_release_date_int) ."         
     q += query_in_filter 
-    print(q)      
     return q
-
 
 
 class MovieFilter:
@@ -133,9 +138,8 @@ class MovieFilter:
         
 
 def generate_query_movie_info(movie_name):
-    q =f"""
-    SELECT DISTINCT ?movie_name ?actor_name ?genre_name ?runtime ?initial_release_date 
-    ?director_name ?producer_name ?language ?writer_name ?editor_name ?music_contributor_name ?country_name
+    q0 = f"""
+    SELECT DISTINCT ?actor_name 
     WHERE{{
         ?movie a lmdb:film ;
             dc:title ?movie_name .
@@ -143,43 +147,129 @@ def generate_query_movie_info(movie_name):
             ?movie lmdb:actor ?actor .
             ?actor lmdb:actor_name ?actor_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+    q1 = f"""
+    SELECT DISTINCT ?genre_name 
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:genre ?genre .
             ?genre lmdb:film_genre_name ?genre_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+    q2 = f"""
+    SELECT DISTINCT ?runtime
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         ?movie lmdb:runtime ?runtime .
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+    
+    q3 = f"""
+    SELECT DISTINCT ?initial_release_date 
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
+        
         ?movie lmdb:initial_release_date ?initial_release_date .
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+
+    q4 = f"""
+    SELECT DISTINCT ?director_name 
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:director ?director .
             ?director lmdb:director_name ?director_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+
+    q5 = f"""
+    SELECT DISTINCT ?producer_name
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:producer ?producer .
             ?producer lmdb:producer_name ?producer_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+    q6 = f"""
+    SELECT DISTINCT ?languageCode
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         ?movie lmdb:language ?language .
+        BIND(STRAFTER(str(?language), "http://www.lingvoj.org/lingvo/") AS ?languageCode)
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+    q7 = f"""
+    SELECT DISTINCT ?writer_name 
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:writer ?writer .
             ?writer lmdb:writer_name ?writer_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+
+    q8 = f"""
+    SELECT DISTINCT ?editor_name
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:editor ?editor .
             ?editor lmdb:editor_name ?editor_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+
+    q9 = f"""
+    SELECT DISTINCT ?music_contributor_name
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:music_contributor ?music_contributor .
             ?music_contributor lmdb:music_contributor_name ?music_contributor_name .
         }}
+        FILTER(?movie_name = "{movie_name}")
+    }}
+    """
+
+    q10 = f"""
+    SELECT DISTINCT ?country_name
+    WHERE{{
+        ?movie a lmdb:film ;
+            dc:title ?movie_name .
         OPTIONAL{{
             ?movie lmdb:country ?country .
             ?country lmdb:country_name ?country_name .
         }}
         FILTER(?movie_name = "{movie_name}")
     }}
-    GROUP BY ?movie_name ?actor_name ?genre_name ?runtime ?initial_release_date 
-    ?director_name ?producer_name ?language ?writer_name ?editor_name ?music_contributor_name ?country_name
     """
-    return q
+    return [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
 
 
 
@@ -194,28 +284,76 @@ def execute_query(g, query):
     return results
 
 
-def transfer_results_to_front_end(results, result_types=["movie_name"]):
+def transfer_results_to_front_end(results, result_types):
     # transfer the results to a list that can be sent to the front end
-    print("transfer the results to a list that can be sent to the front end")
-    # first_result = next(results, None)
-    # print(first_result)
-    # for row in results:
-    #     print(row)
-    results_list = []
-    # print(len(results))
-    # print(results)
+    print("transfer results to front end ------")
+    results_list = {}
+    for result_type in result_types:
+        results_list[result_type] = []
     try:
-        for index, result in enumerate(results):
+        for _, result in enumerate(results):
             for result_type in result_types:
-                results_list.append({index: result[result_type].value})  
+                value = result[result_type].value
+                if value not in results_list[result_type]:
+                    results_list[result_type].append(value)
     except Exception as e:
         print("No results found.", e)
-    print(results_list)
     return results_list
-            
 
+
+
+
+# There are three acitvate functions: 1) for loading graph 2) for filtering 3) for getting movie info
+def activate_initial():         
+    g = load_kg("linkedmdb.nt")
+    return g
+
+
+def activate_filter(front_end_filter_list):
+    filter_dict = create_filter_dict(front_end_filter_list)
+    filter = MovieFilter(filter_dict)
+    filter.display_filter()
+    filter_query = filter.generate_query()
+    results = execute_query(g, filter_query)
+    filter_results = transfer_results_to_front_end(results, result_types=["movie_name"])
+    print(filter_results)
+    return filter_results
+
+def activate_info(front_end_movie_name):
+    info_query = generate_query_movie_info(front_end_movie_name)
+    result_types=["actor_name", "genre_name", "runtime", "initial_release_date", 
+                  "director_name", "producer_name", "languageCode", "writer_name", "editor_name", 
+                  "music_contributor_name", "country_name"]
+    info_result = {}
+    start_time = time.time()
+    for index, query in enumerate(info_query):
+        results = execute_query(g, query)
+        results_dict = transfer_results_to_front_end(results, result_types=[result_types[index]])
+        info_result.update(results_dict)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"whole query execution time: {execution_time}s")
+    print(info_result)
+    return info_result
 
 
 if __name__ == "__main__":
-    # g = load_kg("linkedmdb.nt")
-    pass
+    g = activate_initial()
+    # create actors, genres, runtime, initial_release_date, directors, producers, language, writers, editors, music_contributors, countries numpy arrays
+    actor = np.array(["actor", "Dustin Ho"])
+    genre = np.array(["genre", "Thriller"])
+    runtime = np.array(["runtime", "< 300", "> 100"])
+    initial_release_date = np.array(["initial_release_date", ""])
+    director = np.array(["director", ""])
+    producer = np.array(["producer", ""])
+    language = np.array(["language", ""])
+    writer = np.array(["writer", ""])
+    editor = np.array(["editor", ""])
+    music_contributor = np.array(["music_contributor", ""])
+    country = np.array(["country", ""])
+    front_end_list =[actor, genre, runtime, initial_release_date, director, producer, language, writer, editor, music_contributor, country]
+    
+    activate_filter(front_end_list)
+
+    # activate_info("Buffy the Vampire Slayer")
+
